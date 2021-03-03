fd112af81095d67a860b7fb0f8d5d77bb61a2a4e
Ronny Delgado <delgado.ronny@gmail.com>
2021-03-03 15:27:37 -0600

    Solution for the CyclingRotation challenge

---
+++ Challenges/CyclingRotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class CyclingRotation
+    {
+        static int[] Rotate(int[] originalArray, int numberOfRotation)
+        {
+            int counter = 1;
+            int[] resultingArray = (int[])originalArray.Clone();
+
+            while (counter <= numberOfRotation)
+            {
+                // save last position before the rotation
+                int lastPosition = resultingArray[resultingArray.Length - 1];
+
+                // iterate through the array and move each item one position forward
+                for (int innerCounter = resultingArray.Length - 1; innerCounter >= 1; innerCounter--)
+                {
+                    resultingArray[innerCounter] = resultingArray[innerCounter - 1];
+                }
+
+                resultingArray[0] = lastPosition;
+
+                counter += 1;
+            }
+
+            return resultingArray;
+        }
+    }
+}
