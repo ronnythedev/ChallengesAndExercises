b789f38e0e4b3e8a8c16856fcc1c4d8b0daf244a
Ronny Delgado <delgado.ronny@gmail.com>
2022-02-25 16:38:42 -0600

    - Solved FruitsIntoBaskets using sliding window pattern. Use dictionary instead of Hashtable to avoid boxing and unboxing.

---
+++ ADS/01_SlidingWindow/FruitsIntoBaskets.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADS.SlidingWindow
+{
+    // TL;DR: Get the longest subarray with only two different numbers
+    //
+    // You are visiting a farm that has a single row of fruit trees arranged from left to right.
+    // The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+    //
+    // You want to collect as much fruit as possible.
+    // However, the owner has some strict rules that you must follow:
+    //
+    // * You only have two baskets, and each basket can only hold a single type of fruit.
+    //   There is no limit on the amount of fruit each basket can hold.
+    // * Starting from any tree of your choice, you must pick exactly one fruit from every tree
+    //   (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+    // * Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+    //
+    // Given the integer array fruits, return the maximum number of fruits you can pick.
+    public static class FruitsIntoBaskets
+    {
+        public static int FruitsIntoBasketsSolution(int[] fruits)
+        {
+            var lookup = new Dictionary<int, int>();
+            var lowerIndex = 0;
+            var maxNumberOfFruits = 0;
+
+            for (var upperIndex = 0; upperIndex < fruits.Length; upperIndex++)
+            {
+                var rightFruit = fruits[upperIndex];
+
+                if (lookup.ContainsKey(rightFruit))
+                {
+                    lookup[rightFruit] += 1;
+                }
+                else
+                {
+                    lookup[rightFruit] = 1;
+                }
+
+                while (lookup.Count > 2)
+                {
+                    var leftFruit = fruits[lowerIndex];
+                    lookup[leftFruit] -= 1;
+
+                    if (lookup[leftFruit] == 0)
+                    {
+                        lookup.Remove(leftFruit);
+                    }
+
+                    lowerIndex++;
+                }
+
+                maxNumberOfFruits = Math.Max(maxNumberOfFruits, upperIndex - lowerIndex + 1);
+            }
+
+            return maxNumberOfFruits;
+        }
+    }
+}
---
+++ Tests/01_SlidingWindow/FruitsIntoBasketsTests.cs
@@ -0,0 +1,24 @@
+using ADS.Arrays;
+using ADS.SlidingWindow;
+using NUnit.Framework;
+
+namespace Tests.SlidingWindow
+{
+    public static class FruitsIntoBasketsTests
+    {
+        [TestFixture]
+        public class MaxSubarrayTests
+        {
+            [Test]
+            [TestCase(new[] { 1, 2, 1 }, 3)]
+            [TestCase(new[] { 0, 1, 2, 2 }, 3)]
+            [TestCase(new[] { 1, 2, 3, 2, 2 }, 4)]
+            public void FruitsIntoBasket_WhenCalled_ReturnTheMaxNumberOfFruits(int[] fruits, int expectedResult)
+            {
+                var result = FruitsIntoBaskets.FruitsIntoBasketsSolution(fruits);
+
+                Assert.That(result, Is.EqualTo(expectedResult));
+            }
+        }
+    }
+}
